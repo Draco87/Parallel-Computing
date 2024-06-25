a2883d5ffd804fb902c58bbb1d55febee211fb2c
FARSANA <100363501+Draco87@users.noreply.github.com>
2024-06-25 13:17:04 +0530

    mpi_sum.c

--- mpi_sum.c
+++ mpi_sum.c
@@ -3,21 +3,23 @@
 
 int main(int argc, char** argv) 
 {
-int size, rank,i=0,sum=0,gsum=0;
+    int size, rank, i = 0, sum = 0, gsum = 0;
 
-MPI_Init(&argc,&argv);
-MPI_Comm_size(MPI_COMM_WORLD,&size);
-MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+    MPI_Init(&argc, &argv); // Initialize the MPI environment
+    MPI_Comm_size(MPI_COMM_WORLD, &size); // Get the number of processes
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Get the rank of the process
 
-sum=0;
-for(i=rank*10+1;i<=rank*10+10;i++){
-	sum=sum+i;
-}
+    sum = 0;
+    for(i = rank * 10 + 1; i <= rank * 10 + 10; i++) {
+        sum = sum + i; // Calculate the local sum for each process
+    }
 
-MPI_Reduce(&sum,&gsum,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+    // Reduce all local sums to the global sum at the root process
+    MPI_Reduce(&sum, &gsum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
-printf("rank = %d sum %d   gsum = %d \n",rank, sum, gsum);
-MPI_Finalize();
+    printf("rank = %d sum %d   gsum = %d \n", rank, sum, gsum);
 
-return 0;
+    MPI_Finalize(); // Finalize the MPI environment
+
+    return 0;
 }
